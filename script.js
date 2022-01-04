dc5ac975aee453b73f466d63868fa5ad77cf642c
kausalya17897 <kausalya1787@mail.com>
2022-01-04 11:32:30 +0530

    init

---
+++ script.js
@@ -0,0 +1,36 @@
+document.body.innerHTML=`
+
+
+<section class="user-list">
+
+</section>
+`;
+
+
+
+async function getPopularAnime(){
+    var data=await fetch(`https://animechan.vercel.app/api/quotes/anime?title=naruto&page=2`,{method:"GET"});
+    const users=await data.json();
+    const userContainer=document.querySelector(".user-list");
+    const imagecontain=document.querySelector(".user-piclist")
+    userContainer.innerHTML="";
+    var pic=document.createElement('img');
+    userContainer.append(pic);
+   
+    users.forEach((a)=>{
+        userContainer.innerHTML+=`
+        <div class="user-container">
+       <img src="https://dog.ceo/img/shop/tie-1.jpg" class="user-pic"/>
+        <p class="user-name">Name:${a.anime}</p>
+        <p class="user-character">Character:${a.character}</p>
+        <p class="user-quote">Quote:${a.quote}</p>
+</div>
+        
+        `;
+    });
+    
+
+       
+    console.log(users);
+}
+getPopularAnime();
